605caaf6dda8d7c1728c3e08374c663fc67e5cf7
Emirhan Kılıçaslan <emirhannkilicaslann@gmail.com>
2024-07-08 19:59:32 +0300

    .

--- PaparaHomeworkAPI/obj/Debug/net7.0/PaparaHomeworkAPI.AssemblyInfo.cs
+++ PaparaHomeworkAPI/obj/Debug/net7.0/PaparaHomeworkAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PaparaHomeworkAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5723869ffdfa56da80fd4683fbc382beb1d5aab")]
 [assembly: System.Reflection.AssemblyProductAttribute("PaparaHomeworkAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PaparaHomeworkAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
